283abcd8258a20d3509c7d452f08adcbc98ebb7a
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-10-27 06:54:51 +0300

    Time: 220 ms (78.75%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,7 @@
+-- Write your PostgreSQL query statement below
+SELECT 
+    query_name,
+    ROUND(AVG(rating::numeric/position), 2) quality,
+    ROUND(SUM(CASE WHEN rating < 3 THEN 1 ELSE 0 END)::numeric /COUNT(*) * 100, 2) poor_query_percentage
+FROM Queries
+GROUP BY query_name ;
