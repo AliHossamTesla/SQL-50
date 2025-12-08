47551e2d6bd64e49d32679a09f233ea83d9407d6
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2025-12-08 07:56:10 +0200

    Time: 3 ms (84.99%) | Memory: 18.4 MB (48.34%) - LeetSync

---
+++ 965-unique-email-addresses/unique-email-addresses.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        map<string, set<string>>domains;
+        int numberOfUniqueEmails = 0 ;
+        for(string email : emails){
+            string editedEmail, domain;
+            for(int i = 0 ; i < email.size() ; i ++){
+                if(email[i] == '+'){
+                    while(email[i] != '@') i ++ ;
+                }
+                if(email[i] == '@'){
+                    domain = email.substr(i + 1) ;
+                    break ;
+                }
+                if(email[i] != '.') editedEmail.push_back(email[i]) ;
+            }
+            if(!domains[domain].contains(editedEmail)) numberOfUniqueEmails ++ ;
+            domains[domain].insert(editedEmail) ;
+        }
+        return numberOfUniqueEmails ;
+    }
+};
