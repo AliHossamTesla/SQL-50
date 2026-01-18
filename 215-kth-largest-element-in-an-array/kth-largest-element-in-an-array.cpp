b3e93336469b910619f0fc80d88d365c60fe25d9
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2026-01-18 21:40:54 +0200

    Time: 39 ms (31.92%) | Memory: 61.7 MB (32.88%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.cpp
@@ -1,18 +1,69 @@
+template<bool MAX_HEAP = true>
+class Heap {
+private:
+    vector<int>heap ;
+    int f(int a, int b) {
+        if (MAX_HEAP) return max(a, b );
+        return min(a, b);
+    }
+    int parent(int i) const {return (i - 1)/2;}
+    int left(int i) const {return 2*i + 1;}
+    int right(int i) const {return 2*i + 2;}
+    void siftUp(int i) {
+        while (i > 0 && heap[parent(i)] == f(heap[i], heap[parent(i)])) {
+            swap(heap[i], heap[parent(i)]) ;
+            i = parent(i);
+        }
+    }
+    void siftDown(int i) {
+        int targetIndex = i ;
+        int l = left(i), r = right(i) ;
+        int n = heap.size() ;
+        if (l < n && heap[l] == f(heap[l], heap[targetIndex])) {
+            targetIndex = l;
+        }
+        if (r < n && heap[r] == f(heap[r], heap[targetIndex])){
+            targetIndex = r;
+        }
+        if (targetIndex != i) {
+            swap(heap[i], heap[targetIndex]) ;
+            siftDown(targetIndex) ;
+        }
+    }
+public:
+    Heap(){}
+    Heap(vector<int>& arr) { // O(N)
+        heap = arr;
+        int n = arr.size() ;
+        for (int i = n/2 - 1; i >= 0 ; i --) {
+            siftDown(i) ;
+        }
+    }
+    void push(int v) {
+        heap.push_back(v);
+        siftUp(heap.size() - 1) ;
+    }
+    void pop() {
+        if (heap.empty()) throw runtime_error("Heap is empty") ;
+        heap[0] = heap.back() ;
+        heap.pop_back() ;
+        siftDown(0) ;
+    }
+    int top() const {
+        if (heap.empty()) throw runtime_error("Heap is empty") ;
+        return heap[0] ;
+    }
+    bool empty() const {return heap.empty();}
+    int size() const {return heap.size();}
+};
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        const int SHIFT = 1e4 ;
-        const int MAX_SIZE = 2*SHIFT + 5 ;
-        int count[MAX_SIZE] = {};
-        for(int i : nums){
-            count[i + SHIFT] ++ ;
+        Heap hp(nums) ;
+        while(k-- > 1){
+            hp.pop() ;
         }
-        int current_kth = 0 ;
-        for(int i = MAX_SIZE - 1 ; i >= 0 ; i --){
-            current_kth += count[i] ;
-            if(current_kth >= k) return i - SHIFT ;
-        }
-        assert(0) ;
+        return hp.top() ;
         return -1 ;
     }
 };
