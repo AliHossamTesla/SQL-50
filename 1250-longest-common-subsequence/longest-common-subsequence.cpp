42040496b71b6d9b5103913d1cd81284f7e62b56
Tesla <125549674+AliHossamTesla@users.noreply.github.com>
2026-03-02 07:04:31 +0200

    Time: 19 ms (87.61%) | Memory: 9.1 MB (97.09%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     int longestCommonSubsequence(string s, string t) {
         int n = s.size(), m = t.size() ;
-        vector<int>dp(m + 1) ;
-        for(int i = 1 ; i <= n ; i ++){
-            vector<int>ndp(m + 1) ;
+        vector<vector<int>>dp(2, vector<int>(m + 1)) ;
+        int cnt = 0 ;
+        for(int i = 1; i <= n ; i ++, cnt ^= 1){
             for(int j = 1 ; j <= m ; j ++){
-                if(s[i - 1] == t[j - 1]) ndp[j] = dp[j - 1] + 1 ;
-                else ndp[j] = max(ndp[j - 1], dp[j]) ;
+                if(s[i - 1] == t[j - 1]) dp[cnt][j] = dp[1 - cnt][j - 1] + 1 ;
+                else dp[cnt][j] = max(dp[cnt][j - 1], dp[1 - cnt][j]) ;
             }
-            dp = ndp ;
         }
-        return dp.back() ;
+        return dp[cnt^1].back() ;
     }
 };
